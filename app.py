0c0a0652f647295680d7d192777e9aea85e02966
therealnaveenkamal <therealnaveenkamal@gmail.com>
2022-02-09 22:43:35 +0530

    Final

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
+import flask
 
 app = Flask(__name__)
-
+app.static_folder = 'static'
 
 @app.route('/')
 def hello():
-    return "Hello Python!"
+    return flask.render_template("index.html")
 
 @app.route('/hello')
 def hello1():
     return "You are in Hello!"
 
 if __name__ == '__main__':
-    app.run('localhost', 4449)
+    app.run('localhost', 4449, debug = True)
